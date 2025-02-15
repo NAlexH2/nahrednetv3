5b234fa6896bd718d4adedbe8951fdc87e9765f2
Alex Harris <alex@nah-red.net>
2025-02-14 17:56:46 -0800

    staging for new site

---
+++ src/About.tsx
@@ -0,0 +1,10 @@
+export const About = () => {
+    return (
+        <>
+            <h1>
+                <a href="/">HOME</a>
+            </h1>
+            <div>Hello</div>
+        </>
+    );
+};
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { About } from "./About.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
