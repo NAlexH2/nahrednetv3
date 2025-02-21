2265339bb32f2f5974a3e5c2a4f791d40eace688
Alex Harris <alex@nah-red.net>
2025-02-20 20:14:35 -0800

    adding banner to experience and bio

--- src/routes/experience/Experience.tsx
+++ src/routes/experience/Experience.tsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/navbar/NavBar";
+import TopBanner from "@/components/topBanner/TopBanner";
 
 export const Experience = () => {
   const routedPage: string = "Experience";
 
   return (
     <div className="m-5">
+      <TopBanner />
       <NavBar routedPage={routedPage} />
       experience
     </div>
--- src/routes/projects/Projects.tsx
+++ src/routes/projects/Projects.tsx
@@ -1,10 +1,12 @@
 import NavBar from "@/components/navbar/NavBar";
+import TopBanner from "@/components/topBanner/TopBanner";
 
 export const Projects = () => {
   const routedPage: string = "Projects";
 
   return (
     <div className="m-5">
+      <TopBanner />
       <NavBar routedPage={routedPage} />
       projects
     </div>
