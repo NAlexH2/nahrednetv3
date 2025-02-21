d772259920a577717c58d5975e207fa6e8d8b42f
Alex Harris <alex@nah-red.net>
2025-02-20 19:57:09 -0800

    testing removing header from bio

--- src/routes/bio/Bio.tsx
+++ src/routes/bio/Bio.tsx
@@ -1,13 +1,11 @@
-import Header from "@/components/header/Header";
-import { NavBar } from "@/components/navbar/NavBar";
 import BioContent from "./BioContent";
+import NavBar from "@/components/navbar/NavBar";
 
 export const Bio = () => {
   const routedPage: string = "Bio";
 
   return (
     <div className="m-5">
-      <Header />
       <NavBar routedPage={routedPage} />
       <BioContent />
     </div>
