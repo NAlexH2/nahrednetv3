630d540c65e190a01ec5caf2f01c4833c85c1918
Alex Harris <alex@nah-red.net>
2025-02-20 19:41:35 -0800

    move first paragraph to own file

---
+++ src/routes/bio/FirstParagraphBio.tsx
@@ -0,0 +1,34 @@
+import { IDStyles } from "@/styles";
+import { Link } from "react-router-dom";
+
+export const FirstParagraphBio = () => {
+  return (
+    <>
+      My name is{" "}
+      <i>
+        <b>Nicholas Alexander Harris,</b>
+      </i>{" "}
+      though I go by Alex. I&apos;m currently a graduate student at{" "}
+      <Link
+        to="https://www.pdx.edu/"
+        id={IDStyles.TextLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Portland State University
+      </Link>
+      , where I’m building on the foundation I established during my
+      undergraduate studies at the{" "}
+      <Link
+        to="https://www.pdx.edu/engineering/"
+        id={IDStyles.TextLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Maseeh College of Engineering and Computer Science
+      </Link>
+      . There, I earned a Bachelor of Science in Computer Science and a minor in
+      Mathematics.
+    </>
+  );
+};
