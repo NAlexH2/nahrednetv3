7cc51e3ea8203fe0d4e806d617bc796d8c4f4423
Alex Harris <alex@nah-red.net>
2025-02-20 19:42:38 -0800

    second paragraph to own component

---
+++ src/routes/bio/SecondParagraphBio.tsx
@@ -0,0 +1,9 @@
+export const SecondParagraphBio = () => {
+  const info = `Over the years, friends have described me as "resilient" and "soft-spoken"
+      — qualities I've grown to embrace as reflections of both my character and
+      journey. These traits weren't always a natural part of me, but the
+      challenges I've faced and the experiences I've gained have shaped me into
+      a more grounded and thoughtful individual.`;
+
+  return <>{info}</>;
+};
