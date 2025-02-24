830b436de52a19e061e98ea63bbb4c135ca2446a
NAlexH2 <alex@nah-red.net>
2025-02-24 10:58:18 -0800

    new multiuse button added

---
+++ src/components/buttons/ContentBtn.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import IDStyles from "@/styles/IDStyles.module.css";
+
+interface MButtonProps {
+  btnText: string;
+  textClassName?: string;
+  id?: string;
+}
+const activeBtn: string = `text-jade underline decoration-5 \
+decoration-tp-almond decoration-3`;
+
+const nonActiveBtn: string = `text-tp-almond decoration-5 underline \
+decoration-jade decoration-3`;
+
+export const NavBarBtn = (prop: MButtonProps) => {
+  // const [isAct, setIsAct] = useState(false);
+  // useEffect(() => {
+  //   setIsAct(prop.routedPage === prop.btnText);
+  // }, [prop.routedPage, prop.btnText]);
+
+  // const getButtonClass = (): string => {
+  //   return isAct ? activeBtn : nonActiveBtn;
+  // };
+
+  return (
+    <button id={IDStyles.NavBarBtn}>
+      <div className={`${prop.textClassName}`}>{prop.btnText}</div>
+    </button>
+  );
+};
+
+export default NavBarBtn;
