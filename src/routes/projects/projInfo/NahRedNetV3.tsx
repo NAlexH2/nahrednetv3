ad67e81fc177e6b88914bdb249e2cc1d0e550c16
NAlexH2 <alex@nah-red.net>
2025-03-06 18:05:37 -0800

    Finalizing

--- src/routes/projects/projInfo/NahRedNetV3.tsx
+++ src/routes/projects/projInfo/NahRedNetV3.tsx
@@ -1,12 +1,17 @@
-import { Code, dracula } from "react-code-blocks";
+import { Code, CodeBlock, dracula } from "react-code-blocks";
 import { Link } from "react-router-dom";
 import NRNImage from "@/components/imageComponents/nrnImage";
 import { ClassStyles, IDStyles } from "@/styles";
 
 const projURL = "https://github.com/NAlexH2/nahrednetv3";
+
+const sampleTailwind = `min-md:flex min-md:flex-wrap min-md:justify-center max-md:max-w-80 
+max-md:flex max-md:overflow-x-scroll max-md:min-w-80 max-md:border-l-5 
+max-md:border-r-5 max-md:border-jade max-md:rounded-4xl`;
+
 export const NahRedNetV3 = () => {
   return (
-    <div className="my-8 mx-5">
+    <div className="my-3 mx-5">
       <div className="text-center">
         <Link
           to={projURL}
@@ -24,133 +29,143 @@ export const NahRedNetV3 = () => {
           Tech Stack:
         </div>
         <ul className={`${ClassStyles.CustomList} mb-4`}>
-          <li className="mt-1 ml-5">Python</li>
-          <li className="mt-1 ml-5">Flask/Jinja2</li>
-          <li className="mt-1 ml-5">HTML & Tailwind CSS</li>
+          <li className="mt-1 ml-5">AWS Amplify (Hosting)</li>
           <li className="mt-1 ml-5">
-            {" "}
-            <Link to="https://www.igdb.com/" id={IDStyles.TextLink}>
-              IGDB
-            </Link>{" "}
-            and its{" "}
             <Link
-              to="https://api-docs.igdb.com/#getting-started"
+              to={"https://vite.dev/"}
+              target="_blank"
+              rel="noopener noreferrer"
               id={IDStyles.TextLink}
             >
-              API
-            </Link>{" "}
-          </li>
-          <li className="mt-1 ml-5">
-            {" "}
-            <Link
-              to="https://cloud.google.com/vision?hl=en"
-              id={IDStyles.TextLink}
-            >
-              Google Cloud Vision AI API
+              Vite
             </Link>
           </li>
-          <li className="mt-1 ml-5">Docker</li>
+          <li className="mt-1 ml-5">React</li>
+          <li className="mt-1 ml-5">TypesScript</li>
+          <li className="mt-1 ml-5">
+            <Link
+              to={"https://tailwindcss.com/"}
+              target="_blank"
+              rel="noopener noreferrer"
+              id={IDStyles.TextLink}
+            >
+              Tailwind CSS
+            </Link>
+          </li>
+          <li className="mt-1 ml-5">HTML</li>
+          <li className="mt-1 ml-5">
+            <Link
+              to={"https://www.npmjs.com/package/react-code-blocks"}
+              target="_blank"
+              rel="noopener noreferrer"
+              id={IDStyles.TextLink}
+            >
+              react-code-blocks
+            </Link>
+          </li>
         </ul>
       </p>
       <div className="text-justify">
         <p className="mt-3">
-          This website was put together for a final project during Fall term at
-          Portland State University, and was started the 28th of November 2023
-          and completed on the 6th of December 2023. The only requirements that
-          we had to be concerned with (at the graduate level) was that it must
-          incorporate 2 different API. It is a Flask application hosted on the
-          Google Cloud Platform (GCP) through their Cloud Run service using a
-          docker image built from the source I produced.
+          This website was put together as an update to my previous versions. I
+          elected to rework all of the content and presentation, and started
+          fresh because of this. Even with a fresh start, I was able to bring
+          over a lot of my already created material for the projects section,
+          which a allowed me to also revisit how I wanted to organize and which
+          projects I wanted to showcase. I trimmed down the tools being used so
+          I could learn more by doing more. Getting rid of Material UI and
+          Framer Motion forced me to create components I wanted to use/reuse and
+          learn how to (lightly) animate with CSS - granted, the animation is
+          done with Tailwind CSS, but that itself takes some learning too!
         </p>
         <p className="mt-3">
-          I have had an idea for awhile that I wanted to turn into something
-          more tangible: create a website that allows a person to explore the
-          entirety of video game history. That is a tall order, especially for
-          this final project! Having still wanted to make this, I elected to go
-          a simpler path - allow people to query the Internet Games Database
-          (IGDB) and see information regarding that search.
+          With custom components, to CSS overriding AND customization/extending,
+          and out of the box ready components, this stack allowed me to learn
+          far more than I had expected to. I went into this personal project
+          expecting it to be a lot of placing components and customizing them
+          the way I wanted them to be, but that turned into recreating and
+          building my own components to behave{" "}
+          <i>exactly how I wanted them to</i>
         </p>
-        <p className="mt-3">
-          I had also needed to select another API. During this brainstorming
-          process, the class had just gone through learning how to use Google
-          Cloud AI API, and I figured that generating labels to be used as the
-          alt text on the games cover art would be a good idea. It is/was, but
-          the training data isn&#39;t curated towards games cover art, so the
-          results were mixed.
+        <h3 className="underline decoration-honey mt-3">Why Vite?</h3>
+        <p className="mt-1">
+          Quite simple reasoning actually: I used it before! I knew how to set
+          it up, and customize various features, while learning how to do new
+          ones too (such as aliasing, but that's not necessarily Vite specific.)
+          Unlike previous versions of the website, which started off with raw
+          HTML/CSS with very little JavaScript. Then I created a NextJS version,
+          but found myself stressing over client side rendering and server side
+          rendering, though it provided easy to use features like custom fonts
+          to be incorporated. Not vital to the project, but I wanted it that
+          badly, I found a way! Though, with this version of the site, I'm using
+          Vite as the framework, while being able to use all the features I had
+          with NextJS, and also able to use the React Router to be very explicit
+          about the routing which, was another issue I was having/had with
+          NextJS - just didn't quite trust the automatic nature of it.
         </p>
-        <p className="mt-3">
-          For example: it knows who Mario is, but not Sonic (mostly). So if you
-          look up a Mario and Sonic sports game, the alt text accurately
-          generates for Mario, but not Sonic... Or any of the other characters
-          that appear on the box art, again, mostly.
+        <p className="mt-1">
+          I quickly found myself stuck in a corner with React though! I could
+          not find a clean and easy way to do some things - such as adding
+          custom fonts - in a nice manner. NextJS however, made it super easy to
+          do that! Then, I was running into issues where certain packages I was
+          trying to use were, strangely enough, having issues being compatible.
+          I had thought to myself how strange that was and that it should not
+          matter.
         </p>
-        <h3 className="underline decoration-honey mt-3">Challenges</h3>
+        <h3 className="underline decoration-honey mt-3">On Tailwind CSS</h3>
         <p className="mt-3">
-          The hardest part of this project was parsing the data from the IGDB
-          API after receiving it. Initially, I thought it was just getting the
-          app to appear in the dev environment I was using, but the IGDB API is
-          particularly stale.
+          Now I am quite new to web development, but Tailwind CSS is a dream
+          framework. It made 99% of the work I had to do with CSS incredibly
+          simple. From adding my own extended grid options to use with it, to
+          being able to do something like{" "}
+          {<Code language="TypeScript" text="max-h-[280px]" theme={dracula} />}{" "}
+          where I am setting my own pixel width instead of being constrained to
+          their built in options was amazing! This extends to <i>almost</i> any
+          CSS property too. Here is another pretty cool example:
         </p>
-        <p className="mt-3">
-          When retrieving information, some of the endpoints were
-          non-functional, and so there were workarounds that had to be performed
-          to get the exact data I needed. This included, but not limited to:
-          obtaining the exact release date, the ESRB age rating, and only
-          wanting to view actual real and released games.
-        </p>
-        <p className="mt-3">
-          IGDB allows anyone, it seems, to upload and add a game to the
-          database. This means there are games that are simply fan creations
-          that don&#39;t live on the mainstream. This is fine, but it was
-          interfering with more legitimate results causing these fan creations
-          to have precedence over mainstream releases.
-        </p>
-        <p className="mt-3">
-          I had to develop this workaround to source legit titles because of the
-          limitations I had to set for both the IGDB API and Google Cloud Vision
-          AI API. If the user attempted too many results, the page would
-          eventually timeout instead of waiting for a response.
+        <div className="my-4">
+          <CodeBlock
+            language="TypeScript"
+            text={sampleTailwind}
+            theme={dracula}
+            showLineNumbers={false}
+          />
+        </div>
+        <p className="mt-1">
+          This is a{" "}
+          <Code language="TypeScript" text="className" theme={dracula} /> on a{" "}
+          <Code language="TypeScript" text="<div>" theme={dracula} /> inside of
+          the{" "}
+          <Code language="TypeScript" text="ProjectsContent" theme={dracula} />{" "}
+          component! This example allowed me to specify a maximum media query
+          for an CSS property to stop taking effect. In this case{" "}
+          <Code language="TypeScript" text="max-md" theme={dracula} /> is
+          anything below medium viewport width less than 40rem, or 640px, will
+          take effect. Where as{" "}
+          <Code language="TypeScript" text="min-md" theme={dracula} /> will take
+          effect when the viewport is greater than 40rem/640px, and will persist
+          for all viewport widths beyond that point.
         </p>
         <h3 className="underline decoration-honey mt-3">
-          Jinja Templates Are Pretty Cool!
+          Where Is This Deployed?
         </h3>
         <p className="mt-3">
-          One thing I had discovered working on this project and reading through
-          documentation was that Jinja templates can smartly identify which HTML
-          components to render based on if the response data was returned with
-          or without certain information.
-        </p>
-        <p className="mt-3">
-          In this example, I have a Jinja for loop going through a
-          &#34;games&#34; response object (which has multiple results from the
-          query the user did), and for each game, if{" "}
-          {
-            <Code
-              // @ts-ignore
-              language="TypeScript"
-              text='game["aggregated_rating"]'
-              theme={dracula}
-            />
-          }{" "}
-          has data, then the score - out of 100 - would be displayed.
-        </p>
-        <p className="mt-3">
-          However, if it does not have any data, the page will alert the user
-          that for some reason IGDB currently does not have any critical reviews
-          aggregated for a certain game. I had to do this for the ESRB rating as
-          well, as it is possible for the IGDB to have a released game in their
-          records, but missing the rating for one reason or another.
+          Using a modified build script, the website is built and deployed from
+          AWS Amplify. AWS Amplify can automatically rebuild a website if it
+          detects any changes to the source, much like GitHub actions can too. I
+          also used GitHub pages and GitHub actions to do test deployments just
+          to ensure it looked fine periodically.
         </p>
         <h3 className="underline decoration-honey mt-3">
           How Long Did This Take?
         </h3>
-        <p className="mt-3">
-          I was given approximately 2 weeks to utilize all the knowledge from
-          the class to finish this final project. However, I was finished in a
-          far shorter amount of time, and the project was completed in
-          approximately 6 days. I elected to work ahead of the class material as
-          to give myself as much time needed to complete the project. Easing my
-          future school work debt made this project incredibly manageable!
+        <p>
+          In total, if worked on every day, about a week, off and on. This was a
+          personal project that just took a bit more time though because of
+          other commitments. Though looking at the commit history exclusively,
+          it was 3 weeks. Not bad, and is far less time than the V2, which took
+          me a about 2.5 months of learning and researching on my own simply
+          because of the volume of new stuff I was learning everyday.
         </p>
       </div>
     </div>
